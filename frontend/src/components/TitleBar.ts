0b9b8baf2639e19bbe7564ebdedf625394747e38
Amber <clip@owo.rocks>
2026-03-10 19:23:56 +0100

    Add hidden files + improve dropdown menu

--- frontend/src/components/TitleBar.ts
+++ frontend/src/components/TitleBar.ts
@@ -2,10 +2,12 @@ export class ItemTab {
   id: string;
   label: string;
   view: any;
+  hidden?: boolean;
 
   constructor(id: string, label: string, view: any) {
     this.id = id;
     this.label = label;
     this.view = view;
+    this.hidden = false;
   }
 }
