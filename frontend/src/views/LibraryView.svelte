745e0a681fdf469eb4617f8824819da3ec48990b
Amber <clip@owo.rocks>
2026-03-10 15:50:32 +0100

    Improved placeholder screen

--- frontend/src/views/LibraryView.svelte
+++ frontend/src/views/LibraryView.svelte
@@ -1,6 +1,41 @@
 <script lang="ts">
+    import { MusicNoteIcon } from "phosphor-svelte";
 </script>
 
-<main>This is the library</main>
+<main>
+    <div id="icon">
+        <MusicNoteIcon />
+    </div>
 
-<style></style>
+    <div id="main">Library View</div>
+    <div id="description">This is the placeholder view for the Library</div>
+</main>
+
+<style>
+    main {
+        height: calc(100% - var(--tb-h));
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #icon {
+        font-size: 5em;
+        opacity: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #main {
+        font-weight: 600;
+        font-size: 3em;
+    }
+
+    #description {
+        font-size: 1.25em;
+        opacity: 0.5;
+    }
+</style>
